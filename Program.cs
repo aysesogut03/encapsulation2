d3ed99d8d1e4834ef9cdc861cb7f43a81624b50e
aysesogut03 <aysesogut2003@gmail.com>
2024-12-11 11:24:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            personel personel1=new personel();
+            personel1.KİLO = 57;
+            Console.WriteLine(personel1.KİLO);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ personel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+     public class personel
+    {
+        private int kilo;
+        public int KİLO
+        {
+            get
+            {
+                return kilo;
+            }
+
+            set
+            {
+                kilo = value;
+            }
+        }
+    }
+}
